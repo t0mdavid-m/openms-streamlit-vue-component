c08a67f9a64ff61276c85f50c7f222e65286575a
JeeH-K <jeehyeong91@gmail.com>
2023-08-16 18:33:47 +0200

    Added Vuetify for Sequence view

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,19 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 const app = createApp(App)
 
 app.use(createPinia())
+
+app.use(createVuetify({
+    components,
+    directives,
+}))
 
 app.mount('#app')
